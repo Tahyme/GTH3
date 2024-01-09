b3e7cd7f5fbe46f07c4d5ce3bd730cf3fd38b814
Elaquivala <bombertime4motion@gmail.com>
2024-01-09 14:26:12 +0100

    Add course details and create course functionality

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -3,32 +3,34 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use App\Entity\Event;
 use App\Form\EventType;
 
 
+
 class EventController extends AbstractController
 {
     public function create(Request $request): Response
     {
-        $event = new Event();
-        $form = $this->createForm(EventType::class, $event);
+    $event = new Event();
+    $form = $this->createForm(EventType::class, $event);
 
-        $form->handleRequest($request);
+    $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($event);
-            $entityManager->flush();
+    if ($form->isSubmitted() && $form->isValid()) {
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($event);
+        $entityManager->flush();
 
-            return $this->redirectToRoute('event_show', ['id' => $event->getId()]);
-        }
+        return $this->redirectToRoute('event_show', ['id' => $event->getId()]);
+    }
 
-        return $this->render('event/create.html.twig', [
-            'form' => $form->createView(),
-        ]);
+    return $this->render('event/create.html.twig', [
+        'form' => $form->createView(),
+    ]);
     }
 
     public function show(int $id): Response
